e5eadd89554898175b714f2498cd1ed2dbe4cf3f
sanjaykumarm2001 <158572025+sanjaykumarm2001@users.noreply.github.com>
2024-05-21 01:10:57 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "  "
+  secret_key = " "
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "main" {}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world"
+      image     = "public.ecr.aws/amazonlinux/amazonlinux:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "hello_world" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.subnet.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "hello-world"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
+
+resource "aws_lb" "main" {
+  name               = "hello-world-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs_sg.id]
+  subnets            = [aws_subnet.subnet.id]
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "hello-world-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
